2b11037a454b50faaef1040efbc4074c3b05b133
Anthony K <84693877+K-834@users.noreply.github.com>
2023-10-10 12:10:18 -0500

    reduccion de lineas de cod

--- backendFace.py
+++ backendFace.py
@@ -1,4 +1,3 @@
-import deepface.DeepFace
 import numpy as np
 import cv2
 from ultralytics import YOLO
@@ -19,12 +18,14 @@ verification_cache = {}
 tracker_cache = {}
 unknown_person_detected = False
 unknown_persons_dir = "Personas_Desconocidas"
+
 if not os.path.exists(unknown_persons_dir):
     os.makedirs(unknown_persons_dir)
 
+# Función para enviar un mensaje por WhatsApp
 def send_whatsapp_message(phone_number, message, image_paths):
-    # kit.sendwhats_image(phone_number, audio_path)
-    time.sleep(4)
+    # kit.sendwhats_image(phone_number, audio_path) #implementacion de envio de audio por WhatsApp
+    time.sleep(4) # tiempo de espera
     # Envía las imágenes de personas desconocidas
     for image_path in image_paths:
         kit.sendwhats_image(phone_number, image_path, tab_close=True)
@@ -46,132 +47,87 @@ def check_unknown_person_tracking(tracker_cache, max_tracking_time):
 # modelo identificación de objetos [personas]
 def run1(tipoVideo):
     cap = cv2.VideoCapture(tipoVideo)
-
     model = YOLO("yolov8n.pt")
-
     paused = False
-
     while cap.isOpened():
-
         if not paused:
             status, frame = cap.read()
-
             if not status:
                 break
-
             current_datetime = datetime.datetime.now()
             formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-
             results = model.predict(source=frame, save=True, classes=clase, conf=confi)
-
             frame = results[0].plot()
-
             cv2.putText(frame, formatted_datetime, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
             cv2.imshow("CAMARA VIGILANCIA", frame)
-
             for i, result in enumerate(results):
-                # Guarda cada imagen detectada en la carpeta "runs/detect"
-                image_name = f"detected_{i}.jpg"  # Nombre de archivo para la imagen detectada
+                image_name = f"detected_{i}.jpg"
                 image_path = os.path.join("runs", "deteccion", image_name)
                 cv2.imwrite(image_path, result.plot())
-
         key = cv2.waitKey(1)
-
         if key == 27:
             break
         elif key == 32:
             paused = not paused
-
     print("Eso es todo")
     cap.release()
     cv2.destroyAllWindows()
 
-# modelo verificación de personas por rostros
+# modelo trackeo de personas
 def run2(tipoVideo):
     cap = cv2.VideoCapture(tipoVideo)
-
     model = YOLO("yolov8n.pt")
-
     tracker = Sort()
-
     current_datetime = datetime.datetime.now()
     formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-
     while cap.isOpened():
         status, frame = cap.read()
-
         if not status:
             break
-
         results = model(frame, stream=True,classes=clase)
-
         for res in results:
             filtered_indices = np.where(res.boxes.conf.cpu().numpy() > confi)[0]
             boxes = res.boxes.xyxy.cpu().numpy()[filtered_indices].astype(int)
             tracks = tracker.update(boxes)
             tracks = tracks.astype(int)
-
             for xmin, ymin, xmax, ymax, track_id in tracks:
                 cv2.putText(img=frame, text="Desconocido", org=(xmin, ymin - 10), fontFace=cv2.FONT_HERSHEY_PLAIN,
                             fontScale=1.0, color=(0, 0, 255), thickness=2)
-
                 cv2.rectangle(img=frame, pt1=(xmin, ymin), pt2=(xmax, ymax), color=(0, 0, 255), thickness=2)
-
-            # frame = results[0].plot()
-
-
-
-
         cv2.putText(frame, formatted_datetime, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
         cv2.imshow("CAMARA VIGILANCIA", frame)
-
-
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
     cap.release()
 
-
-# modelo tracking de desconocidos y envio de mensajes
+# modelo tracking de desconocidos y reconocimiento facial
 def run3(tipoVideo):
     cap = cv2.VideoCapture(tipoVideo)
-
     # Inicializa el modelo YOLOv8n de Ultralytics
     model = YOLO("yolov8n.pt")
-
     # Inicializa el tracker de personas (Sort)
     tracker = Sort()
-
     current_datetime = datetime.datetime.now()
     formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-
     # Directorio de la base de datos de caras conocidas
-
     while cap.isOpened():
         status, frame = cap.read()
-
         if not status:
             break
-
         # Realizar la detección de objetos con YOLOv8n
         results = model(frame, stream=True, classes=clase)
-
         for res in results:
             filtered_indices = np.where(res.boxes.conf.cpu().numpy() > confi)[0]
             boxes = res.boxes.xyxy.cpu().numpy()[filtered_indices].astype(int)
             tracks = tracker.update(boxes)
             tracks = tracks.astype(int)
-
             for xmin, ymin, xmax, ymax, track_id in tracks:
                 # Extraer la región de interés (ROI) de la persona detectada
                 roi = frame[ymin:ymax, xmin:xmax]
-
                 # Crear un archivo temporal para la ROI
                 roi_path = "temp_roi.png"
                 cv2.imwrite(roi_path, roi)
-
                 # Verificar si el archivo temporal existe
                 if os.path.exists(roi_path):
                     # Comprueba si el track_id ya está en la caché de verificación
@@ -187,19 +143,14 @@ def run3(tipoVideo):
                             # Usar los archivos temporales creados
                             verification_result = DeepFace.verify(img1_path=roi_path, img2_path=db_image_path,enforce_detection=False)
                             verification_results.append(verification_result)
-
                         # Almacenar el resultado en la caché de verificación
                         verification_cache[track_id] = verification_results
-
                     # Eliminar el archivo temporal de la ROI
                     os.remove(roi_path)
-
                     # Comprobar si la persona es conocida o desconocida
                     min_distance = min(result['distance'] for result in verification_results)
-
                     # Definir un umbral de compatibilidad
                     umbral_de_compatibilidad = 0.5  # Puedes ajustar este valor según tus necesidades
-
                     # Dibujar la etiqueta en el cuadro junto con el nombre de la imagen de la base de datos
                     filename = os.path.basename(db_image_path)
                     # Comprobar si la persona es conocida o desconocida
@@ -209,28 +160,21 @@ def run3(tipoVideo):
                     else:
                         label = f"Desconocido ({track_id})"
                         color = (0, 0, 255)
-
                     cv2.putText(img=frame, text=label, org=(xmin, ymin - 10),
                                 fontFace=cv2.FONT_HERSHEY_PLAIN,
                                 fontScale=1.0, color=color, thickness=2)
-
                     cv2.rectangle(img=frame, pt1=(xmin, ymin), pt2=(xmax, ymax), color=color, thickness=2)
-
         cv2.putText(frame, formatted_datetime, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
         cv2.imshow("CAMARA VIGILANCIA", frame)
-
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
     cap.release()
     cv2.destroyAllWindows()
 
-
-# Función para enviar un mensaje por WhatsApp
-
 # Función para el procesamiento de OpenCV en segundo plano
 def opencv_processing(tipoVideo):
+    # Uso del globlal para la variable de deteccion de personas desconocidas
     global unknown_person_detected
 
     cap = cv2.VideoCapture(tipoVideo)
@@ -363,13 +307,10 @@ def opencv_processing(tipoVideo):
 
 def VerificacionSesion():
     cap = cv2.VideoCapture(0)
-
-    DeepFace.stream(db_path="database")
-
+    DeepFace.stream(db_path="database") #libreria para el reococimiento de emociones y rostro con la libereria deepFace
     while True:
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
-
     cap.release()  # Libera la cámara
     cv2.destroyAllWindows()  # Cierra las ventanas de OpenCV
 
--- run.py
+++ run.py
@@ -6,11 +6,13 @@ if __name__ == "__main__":
 
     metodo = input("Ingresa el número del método que deseas utilizar: ")
 
-    if metodo == "1":
+    if metodo == "1": #camara de la laptop
         tipoVideo = 0
-    elif metodo == "2":
+
+    elif metodo == "2": #video descargado de muestra
         tipoVideo = "prueba/ejemplo2.mp4"
-    elif metodo == "3":
+
+    elif metodo == "3": # celular convertida en camara IP
         ip_address = "192.123.231.1"
         port = 8080
         tipoVideo = f"http://{ip_address}:{port}/video"
